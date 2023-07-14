a30497b8a3b608af437b564926259410f96ae715
mariodac <mdac.mario@gmail.com>
2023-07-14 20:57:12 -0300

    Templates, arquivos estáticos criados

--- edu/views.py
+++ edu/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Matrículas</h1>')
+    dados_dos_institutos = {
+        'IF Goiano': 'Goiás',
+        'IFSP' : 'São Paulo',
+        'IFMG' : 'Minas Gerais',
+    }
+    dados = {
+        'dados_dos_institutos': dados_dos_institutos
+    }
+    return render(request, 'index.html', dados)
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
