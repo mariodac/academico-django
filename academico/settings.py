09b2a691f0fbc1aa8fa037b9f758e159b68ff7a4
mariodac <mdac.mario@gmail.com>
2023-07-15 00:03:28 -0300

    Configurando banco de dados

--- academico/settings.py
+++ academico/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'academico.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'db_academico',
+
+        'USER': 'academico',
+
+            'PASSWORD': '123qweasd',
+
+        'HOST': 'localhost',
+
     }
+
 }
 
 
