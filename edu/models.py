577283ca73683ab03d242be7e321b82c53262c0e
mariodac <mdac.mario@gmail.com>
2023-07-15 00:12:12 -0300

    Criado modelo de matricula

--- edu/models.py
+++ edu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Matricula(models.Model):
+    matricula = models.IntegerField()
+    nome_aluno = models.CharField(max_length=200)
+    curso = models.CharField(max_length=100)
+    observacao = models.TextField()
+    data_matricula = models.DateTimeField(verbose_name="Data da matrícula", blank=True, auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Matricula de Aluno"
+        verbose_name_plural = "Matricula de Alunos"
+        ordering = ["nome_aluno"]
+
